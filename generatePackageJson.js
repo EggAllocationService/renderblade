47a6db5e7356edbebed4656a7afa6fc422585937
Kyle Smith <kyle@cabotmc.dev>
2025-02-17 23:09:10 -0400

    update release script

--- generatePackageJson.js
+++ generatePackageJson.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import os from 'os';
 
 const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
@@ -7,5 +8,6 @@ delete packageJson.dependencies["tweakpane"];
 packageJson.browser = "renderblade.js"
 packageJson.types = "renderblade.d.ts"
 packageJson.private = "false"
+packageJson.version = process.argv[2];
 
 fs.writeFileSync('./dist/package.json', JSON.stringify(packageJson, null, 2));
