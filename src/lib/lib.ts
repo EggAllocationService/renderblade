1646743d72fcbfebf4765d9f29f0d53b766c5512
Kyle Smith <kyle@cabotmc.dev>
2025-02-17 22:48:05 -0400

    Add library generation

---
+++ src/lib/lib.ts
@@ -0,0 +1,11 @@
+
+export { Camera } from "./Camera";
+export { FBO } from "./FBO";
+export { Material, TextureTarget } from "./Material";
+export { Program } from "./Program";
+export { SimpleMaterial } from "./SimpleMaterial";
+export { fetchTexture } from "./util";
+export { PostEffect } from "./PostEffect";
+export { Object3D } from "./Object3D";
+export { Pane } from "tweakpane";
+
